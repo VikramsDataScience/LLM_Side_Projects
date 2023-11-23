9aa308f5299ed5a5736250aac9d5c05abe95333a
Vikram Pande <vikrampande28@gmail.com>
2023-11-23 12:30:02 +1100

    Strip Unicode Preprocessing component

--- src_preprocessing/Job_Ad_Q&A_PreProcessing.py
+++ src_preprocessing/Job_Ad_Q&A_PreProcessing.py
@@ -3,7 +3,6 @@ from torch import cuda
 from tqdm.auto import tqdm
 import json
 from pathlib import Path
-from datasets import load_dataset # ! pip install datasets
 from bs4 import BeautifulSoup
 
 # Verify if GPU is being used
@@ -21,18 +20,26 @@ with open(Path(raw_data_path) / 'ads-50k.json', 'r', encoding='utf-8') as f:
 # Declare a function to strip HTML tags and perform some other Pre-processing
 def stripTags(input_string):
 
-    soup = BeautifulSoup(input_string, 'html.parser')
-    string = input_string.replace('.', '. ')
+    string = input_string.encode("ascii", "ignore")
+    string = string.decode()
+    string = BeautifulSoup(input_string, 'html.parser')
+    string = string.get_text(strip=True)
+    string = (string.replace('\u2022', '').replace('\u00a0', ' ').replace('\u2019', '').replace('\u00b7', '')
+              .replace('\u2013', '').replace('\u2026', '').replace('\u200b', '').replace('\u2018', '')
+              .replace('\u201c', '').replace('\u201d', '').replace('\u00e9', 'e').replace('\uff1a', '')
+              .replace('\u00bd', '').replace('\u0101', '').replace('\u0113', '').replace('\u016b', 'ū')
+              .replace('\u014d', 'ō').replace('\n', ' ').replace('\u00ae', '').replace('\u2122', '')
+              .replace('\u00ad', '').replace('\\', ''))
 
-    return soup.get_text(strip=True), string
+    return string
 
 # Strip HTML tags from the 'content' object and append it to a new JSON dictionary 
 # Since the json.dump() call in the loop is an 'append' statement, if the file exists delete it. Otherwise, the json.dump() call will append the dictionary without limit (i.e. objects will duplicate)
 if path.exists(Path(raw_data_path) / 'content_cleaned.json'):
     remove(Path(raw_data_path) / 'content_cleaned.json')
 
+# Recursively strip HTML tags and display a progress bar in the for loop
 for i in tqdm(range(0, len(raw_data)), desc='Data Pre-Processing Progress'):
-
     content_cleaned = {
         'content': stripTags(raw_data[i]['content'])
     }
