1775a122a2e60b4df15b8cb6b7cb827c148a35be
Vikram Pande <vikrampande28@gmail.com>
2023-11-29 13:02:29 +1100

    Recommender Preprocessing component created and README file modified

--- Recommender_Model/README.md
+++ Recommender_Model/README.md
@@ -1,1 +1,9 @@
-# Shopping Cart Recommender Model
+# Shopping Cart Recommender Model (WIP)
+This project is a recommender model based on ecommerce orders data (including whether certain products were reordered).
+
+# EDA
+## On Missing Values
+The `ProfileReport` seemingly exposes about 10,565 missing values from the dataset. However, the missing values appear to be visits to particular product pages, but where no orders were made:
+![Alt text](image.png)
+We can see that there are missing rows in the columns `'order_id'`, `'add_to_cart_order'`, and `'reordered'` - all of these columns appear to relate to orders placed. Being as this is a recommender, I felt it would be prudent to keep these rows, since they contain important information on how users are navigating the site.
+
--- Recommender_Model/src/Preprocessing_EDA/Preprocessing_EDA.py
+++ Recommender_Model/src/Preprocessing_EDA/Preprocessing_EDA.py
@@ -1,10 +1,11 @@
+from os import path
 import pandas as pd
-from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from pathlib import Path
+from ydata_profiling import ProfileReport
 
 # Define file paths
 files_path = Path('C:/Sample Data')
+preprocess_path = Path('C:/Users/Vikram Pande/LLM_Side_Projects/Recommender_Model/src/Preprocessing_EDA')
 
 # Read in the individual csv files, and store as separate Pandas Dataframes
 aisles = pd.read_csv(files_path / 'aisles.csv')
@@ -20,3 +21,13 @@ merged_products = pd.merge(merged_products, order_products__train, on='product_i
 merged_products = merged_products.reindex(columns=['order_id','product_id','product_name','aisle_id','aisle','department_id','department', 'add_to_cart_order', 'reordered'])
 print(merged_products)
 
+# If the EDA report doesn't exist, conduct EDA by generating a ydata Profile Report
+if not path.exists(preprocess_path / 'EDA_Profile_Report.html'):
+    profile_report = ProfileReport(merged_products, tsmode=False, explorative=True, dark_mode=True)
+    profile_report.to_file(preprocess_path / 'EDA_Profile_Report.html')
+
+# If there are missing values, assess where these values exist
+print(merged_products.isna().sum())
+
+# Save Preprocessed data into a compressed Parquet DF for downstream consumption
+merged_products.to_parquet(files_path / 'merged_products.parquet.gz', index=False, compression='gzip')
