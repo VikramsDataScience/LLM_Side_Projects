049418aa776c8998050897d8d3ab315519ec6b41
Vikram Pande <vikrampande28@gmail.com>
2023-12-12 13:54:44 +1100

    Modified Preprocessing and Model components

--- Recommender_Model/src/Preprocessing_EDA/Preprocessing_EDA.py
+++ Recommender_Model/src/Preprocessing_EDA/Preprocessing_EDA.py
@@ -26,33 +26,36 @@ merged_products = pd.merge(merged_products, order_products_train, on='product_id
 merged_products = merged_products.reindex(columns=['order_id','product_id','product_name','aisle_id','aisle','department_id','department', 'add_to_cart_order', 'reordered'])
 print('*****FINAL DF:*****' + '\n', merged_products)
 
-# Define function for generating a sparse matrix
-def sparse_matrix(order_ids, item_ids):
+# Define function for generating a sparse coordinate matrix
+def sparse_matrix(order_ids, item_ids, reordered):
     sparse_df['order_id'] = orders['order_id'].map(order_ids)
+    sparse_df['reordered'] = orders['reordered'].map(reordered)
     sparse_df['product_id'] = merged_products['product_id'].map(item_ids)
     
     # Replace NaN values with a placeholder (e.g., 0)
     sparse_df['order_id'] = sparse_df['order_id'].fillna(0)
+    sparse_df['reordered'] = sparse_df['reordered'].fillna(0)
     sparse_df['product_id'] = sparse_df['product_id'].fillna(0)
     
     # Cast as integers
     sparse_df['order_id'] = sparse_df['order_id'].astype(int)
+    sparse_df['reordered'] = sparse_df['reordered'].astype(int)
     sparse_df['product_id'] = sparse_df['product_id'].astype(int)
-    print('NUM_ORDERS:' + '\n', sparse_df['order_id'])
-    print('NUM_PRODUCTS:'+ '\n', sparse_df['product_id'])
+    print('SPARSE_DF:' + '\n', sparse_df)
     
-    # Generate Sparse Matrix
-    sp_matrix = sp.coo_matrix((np.ones(len(sparse_df)), (sparse_df['order_id'], sparse_df['product_id'])))
+    # Generate a Coordinate Sparse Matrix
+    sp_matrix = sp.coo_matrix((sparse_df['reordered'], (sparse_df['order_id'], sparse_df['product_id'])))
+    print(sp_matrix)
 
     # Save Sparse Matrix into a compressed Numpy format '.npz' for downstream consumption
     sp.save_npz(files_path / 'sparse_matrix_v0.0.1.npz', sp_matrix)
-    sparse_df.to_parquet(files_path / 'sparse_df.gz', compression='gzip')
     return sp_matrix
 
 # Perform preprocessing for Neural Collaborative Filtering and save Sparse Matrix for downstream consumption
 order_ids = {order_ids: idx for idx, order_ids in enumerate(orders['order_id'].unique())}
+reordered = {reordered: idx for idx, reordered in enumerate(orders['reordered'].unique())}
 item_ids = {item_ids: idx for idx, item_ids in enumerate(merged_products['product_id'])}
-sparse_matrix(order_ids, item_ids)
+sparse_matrix(order_ids, item_ids, reordered)
 
 # If the EDA report doesn't exist, conduct EDA by generating a ydata Profile Report
 if not path.exists(preprocess_path / 'EDA_Profile_Report.html'):
