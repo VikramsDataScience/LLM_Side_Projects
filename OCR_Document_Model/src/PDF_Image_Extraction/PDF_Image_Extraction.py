865ccc30fe776348859344cefac62a61db116b1a
Vikram Pande <vikrampande28@gmail.com>
2024-01-16 14:23:44 +1100

    Image Extraction module improvements

--- OCR_Document_Model/src/PDF_Image_Extraction/PDF_Image_Extraction.py
+++ OCR_Document_Model/src/PDF_Image_Extraction/PDF_Image_Extraction.py
@@ -1,6 +1,6 @@
-import pytesseract
 import fitz # PyMuPDF for PDF and image data extraction
 from torchvision import transforms
+import PIL
 from PIL import Image
 from pathlib import Path
 from os import listdir, path, makedirs
@@ -35,53 +35,44 @@ id = start_id
 
 # Function to preprocess image using PyTorch transforms
 def preprocess_image(image_path):
-    image = Image.open(image_path).convert('RGB')
-    transform = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ])
-    input_tensor = transform(image)
-    return input_tensor.unsqueeze(0)
+    try:
+        image = Image.open(image_path).convert('RGB')
+    except PIL.UnidentifiedImageError:
+        pass
+    else:
+        transform = transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+        input_tensor = transform(image)
+        return input_tensor.unsqueeze(0)
 
-def extract_images_from_pdf(output_path):
+def extract_images_from_pdf(files_path, extracted_images_path):
+    pdf_document = fitz.open(files_path)
+    for page_number in range(pdf_document.page_count):
+        page = pdf_document[page_number]
+        images_list = page.get_images(full=True)
+
+        for img_index, img_info in enumerate(images_list):
+            img_id = img_info[0]
+            base_image = pdf_document.extract_image(img_id)
+            image_bytes = base_image['image']
+            image = Image.open(io.BytesIO(image_bytes))
+            # Save to storage location (each image will represent one page)
+            pdf_filename = path.splitext(path.basename(files_path))[0] # Extract filename without .pdf extension
+            image.save(open(Path(extracted_images_path) / f'docid_{pdf_filename}_page_{page_number + 1}_img_{img_index}.png', 'w'))
+
+# Iterate and load PDF documents, read all pages in document & extract images
+for pdf_file in tqdm(listdir(files_path), desc='PDF Image Extraction Progress'):
     # If the output folder doesn't exist, create folder
-    if not path.exists(output_path):
-        makedirs(output_path)
+    if not path.exists(extracted_images_path):
+        makedirs(extracted_images_path)
     
-    # Load PDF document, read all pages in document & extract images
-    for pdf_file in tqdm(listdir(f'{files_path}/0{id:.4f}.pdf'), desc='PDF Image Extraction Progress'):
-        try:
-            pdf_document = fitz.open(pdf_file)
-        except fitz.fitz.FileNotFoundError: # Handle any FileNotFoundErrors caused by research papers being removed from arXiv server
-            pass
-        else:
-            for page_number in range(pdf_document.page_count):
-                page = pdf_document[page_number]
-                images = page.get_images(full=True)
-
-                for img_index, img_info in enumerate(images):
-                    img_index += 1
-                    img_index = img_info[0]
-                    base_image = pdf_document.extract_image(img_index)
-                    image_bytes = base_image['image']
-                    image = Image.open(io.BytesIO(image_bytes))
-
-                    # Perform image Preprocessing
-                    image_preprocess = preprocess_image(f'{files_path}/0{id:.4f}.pdf')
-
-                    # Save to storage location
-                    save_path = path.join(output_path, f'page_{page_number + 1}_img_{img_index}.png')
-                    image_preprocess.save(save_path)
-
-                pdf_document.close()
-        
-        id += 0.0001
-
-extract_images_from_pdf(extracted_images_path)
-
-# Function to perform OCR using Tesseract on an image
-def perform_ocr(image_path):
-    image = Image.open(image_path)
-    text = pytesseract.image_to_string(image)
-    return text
+    try:
+        id = start_id
+        pdf_images = extract_images_from_pdf(f'{files_path}/0{id:.4f}.pdf', extracted_images_path)
+    except fitz.fitz.FileNotFoundError: # Handle any FileNotFoundErrors caused by research papers being removed from arXiv server
+        pass
+    else:
+        id += 0.0001    
