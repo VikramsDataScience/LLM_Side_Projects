1ec94fd69f8b280f3ebd33985bb7960eda5d88fe
Vikram Pande <vikrampande28@gmail.com>
2024-01-22 12:34:10 +1100

    Preprocessing module function conversion to class methods

--- OCR_Document_Model/src/PDF_Preprocessing/PDF_Preprocessing.py
+++ OCR_Document_Model/src/PDF_Preprocessing/PDF_Preprocessing.py
@@ -32,81 +32,236 @@ preprocessed_images_path = global_vars['preprocessed_images_path']
 preprocessed_image_width = global_vars['preprocessed_image_width']
 preprocessed_image_height = global_vars['preprocessed_image_height']
 
-############# DEFINE IMAGE PREPROCESSING FUNCTIONS AND SAVE TO STORAGE #############
-def load_image(image_path):
-    return cv2.imread(image_path)
+############# DEFINE IMAGE PREPROCESSING CLASS WITH ASSOCIATED METHODS #############
+class ImagePreprocessing:
+    @staticmethod # Use '@staticmethod' decorator to remove any dependencies on the instance state (i.e. remove the requirement for a 'self' parameter in the class)
+    def load_image(image_path):
+        return cv2.imread(image_path)
 
-def save_image(image, output_path):
-    cv2.imwrite(output_path, image)
+    @staticmethod
+    def rescale_image(image, new_width, new_height):
+        """
+        Rescales the input image to the specified width and height.
 
-def rescale_image(image, new_width, new_height):
-    return cv2.resize(image, (new_width, new_height))
+        Parameters:
+        - image (numpy.ndarray): Input image.
+        - new_width (int): Width of the rescaled image.
+        - new_height (int): Height of the rescaled image.
 
-def convert_to_grayscale(image):
-    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        Returns:
+        numpy.ndarray: Rescaled image.
+        """
+        return cv2.resize(image, (new_width, new_height))
 
-def reduce_noise(image):
-    return cv2.GaussianBlur(image, (5,5), 0)
+    @staticmethod
+    def reduce_noise(image):
+        """
+        In order to separate the noise in the images, apply blurring. Sharper images lose their detail, so using a low-pass filter
+        has the effect of reducing the amount of noise and detail in the image.
+        
+        Applies Gaussian blur to the input image for noise reduction.
 
-def apply_threshold(image, threshold_value=128):
-    _, binary_image = cv2.threshold(image, threshold_value, 255, cv2.THRESH_BINARY)
-    return binary_image
+        Parameters:
+        - image (numpy.ndarray): Input image.
 
-def enhance_contrast(image):
-    return exposure.equalize_hist(image)
+        Returns:
+        numpy.ndarray: Image with reduced noise.
+        """
+        return cv2.GaussianBlur(image, (5,5), 0)
 
-def deskew_image(image):
-    coords = np.column_stack(np.where(image > 0))
-    angle = cv2.minAreaRect(coords)[-1]
-    if angle < -45:
-        angle = -(90 + angle)
-    else:
-        angle = -angle
-    (h, w) = image.shape[:2]
-    centre = (w // 2, h // 2)
-    rotation_matrix = cv2.getRotationMatrix2D(centre, angle, 1.0)
-    rotated_image = cv2.warpAffine(image, rotation_matrix, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
-    return rotated_image
+    @staticmethod
+    def convert_to_grayscale(image):
+        """
+        Transformations within RGB space like adding/removing the alpha channel, reversing the channel order, 
+        conversion to/from 16-bit RGB color (R5:G6:B5 or R5:G5:B5), as well as conversion to/from grayscale.
 
-def apply_adaptive_threshold(image):
-    return cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
+        Parameters:
+        - image (numpy.ndarray): Input color image.
 
-def localize_text(image, kernel_size=(5,5)):
-    kernel = np.ones(kernel_size, np.uint8)
-    return cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
+        Returns:
+        numpy.ndarray: Grayscale image.
+        """
+        return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-def resample_image(image, new_width, new_height):
-    return cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_AREA)
+    @staticmethod
+    def edge_detection(image, lower_threshold=50, upper_threshold=150):
+        """
+        Edge detection works better when the image is first converted to grayscale and smoothed or blurred 
+        (as has been done in the convert_to_grayscale() & reduce_noice() functions).
 
-def normalize_image(image):
-    return image.astype(np.float32) / 255.0
+        Performs edge detection on the input image using the Canny edge detector.
 
-def rotate_image(image, angle=15):
-    return transform.rotate(image, angle=angle)
+        Parameters:
+        - image (numpy.ndarray): Input image.
+        - lower_threshold (int): Lower threshold for edge detection. Default is 50.
+        - upper_threshold (int): Upper threshold for edge detection. Default is 150.
+
+        Returns:
+        numpy.ndarray: Image with detected edges.
+        """
+        return cv2.Canny(image, lower_threshold, upper_threshold)
+
+    @staticmethod
+    def apply_threshold(image, threshold_value=128):
+        """
+        Applies a fixed-level threshold to each array element.
+
+        The function applies fixed-level thresholding to a multiple-channel array. The function is typically used to 
+        get a bi-level (binary) image out of a grayscale image ( compare could be also used for this purpose) or for 
+        removing a noise, that is, filtering out pixels with too small or too large values. There are several types of thresholding supported by the function. 
+        They are determined by type parameter.
+
+        Also, the special values THRESH_OTSU or THRESH_TRIANGLE may be combined with one of the above values. 
+        In these cases, the function determines the optimal threshold value using the Otsu's or Triangle algorithm and uses it instead of the specified thresh.
+
+        Note: Currently, the Otsu's and Triangle methods are implemented only for 8-bit single-channel images.
+        Parameters:
+        src	input array (multiple-channel, 8-bit or 32-bit floating point).
+        dst	output array of the same size and type and the same number of channels as src.
+        - thresh:	threshold value.
+        - maxval:	maximum value to use with the THRESH_BINARY and THRESH_BINARY_INV thresholding types.
+        - type:	thresholding type (see ThresholdTypes).
+        Returns:
+        The computed threshold value if Otsu's or Triangle methods are used.
+        """
+        _, binary_image = cv2.threshold(image, threshold_value, 255, cv2.THRESH_BINARY)
+        return binary_image
+
+    @staticmethod
+    def enhance_contrast(image):
+        """
+        Enhances the contrast of the input image using histogram equalization.
+
+        Parameters:
+        - image (numpy.ndarray): Input image.
+
+        Returns:
+        numpy.ndarray: Image with enhanced contrast.
+        """
+        return exposure.equalize_hist(image)
+
+    @staticmethod
+    def deskew_image(image):
+        """
+        Deskews the input image by rotating it to align with the predominant text orientation.
+
+        Parameters:
+        - image (numpy.ndarray): Input image containing text.
+
+        Returns:
+        numpy.ndarray: Deskewed image.
+        """
+        coords = np.column_stack(np.where(image > 0))
+        angle = cv2.minAreaRect(coords)[-1]
+        if angle < -45:
+            angle = -(90 + angle)
+        else:
+            angle = -angle
+        (h, w) = image.shape[:2]
+        centre = (w // 2, h // 2)
+        rotation_matrix = cv2.getRotationMatrix2D(centre, angle, 1.0)
+        rotated_image = cv2.warpAffine(image, rotation_matrix, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+        return rotated_image
+
+    @staticmethod
+    def apply_adaptive_threshold(image):
+        """
+        Applies adaptive thresholding to the input image.
+
+        Parameters:
+        - image (numpy.ndarray): Input image.
+
+        Returns:
+        numpy.ndarray: Thresholded image.
+        """
+        return cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
+
+    @staticmethod
+    def localize_text(image, kernel_size=(5,5)):
+        """
+        Localizes text in the input image using morphological operations.
+
+        Parameters:
+        - image (numpy.ndarray): Input image.
+        - kernel_size (tuple): Size of the structuring element kernel. Default is (5, 5).
+
+        Returns:
+        numpy.ndarray: Image with localized text.
+        """
+        kernel = np.ones(kernel_size, np.uint8)
+        return cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
+
+    @staticmethod
+    def resample_image(image, new_width, new_height):
+        """
+        Resamples the input image to the specified dimensions using interpolation.
+
+        Parameters:
+        - image (numpy.ndarray): Input image.
+        - new_width (int): Width of the resampled image.
+        - new_height (int): Height of the resampled image.
+
+        Returns:
+        numpy.ndarray: Resampled image.
+        """
+        return cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_AREA)
+
+    @staticmethod
+    def normalize_image(image):
+        """
+        Normalizes pixel values of the input image to the range [0, 1].
+
+        Parameters:
+        - image (numpy.ndarray): Input image.
+
+        Returns:
+        numpy.ndarray: Normalized image with pixel values in the range [0, 1].
+        """
+        return image.astype(np.float32) / 255.0
+
+    @staticmethod
+    def rotate_image(image, angle=15):
+        """
+        Rotate the given image by the specified angle.
+
+        Parameters:
+        - image: The input image.
+        - angle: The angle of rotation in degrees (default is 15 degrees).
+
+        Returns:
+        The rotated image.
+        """
+        return transform.rotate(image, angle=angle)
+
+############# INTANTIATE CLASS AND CALL IMAGE PREPROCESSING STEPS IN THEIR RESPECTIVE ORDER #############
+ImgPreprocess = ImagePreprocessing()
 
 def preprocess_and_save(image_path, output_folder):
-    image = load_image(image_path)
+    image = ImgPreprocess.load_image(image_path)
 
-    # Apply preprocessing steps
-    preprocessed_image = convert_to_grayscale(image)
-    preprocessed_image = reduce_noise(preprocessed_image)
-    preprocessed_image = apply_threshold(preprocessed_image)
-    preprocessed_image = enhance_contrast(preprocessed_image)
-    preprocessed_image = deskew_image(preprocessed_image)
-    preprocessed_image = apply_adaptive_threshold(preprocessed_image)
-    preprocessed_image = localize_text(preprocessed_image)
-    preprocessed_image = resample_image(preprocessed_image, new_width, new_height)
-    preprocessed_image = normalize_image(preprocessed_image)
+    # Apply preprocessing steps in order
+    preprocessed_image = ImgPreprocess.rescale_image(image, new_width=preprocessed_image_width, new_height=preprocessed_image_height)
+    preprocessed_image = ImgPreprocess.reduce_noise(preprocessed_image)
+    preprocessed_image = ImgPreprocess.convert_to_grayscale(preprocessed_image)
+    preprocessed_image = ImgPreprocess.edge_detection(preprocessed_image)
+    preprocessed_image = ImgPreprocess.apply_threshold(preprocessed_image)
+    preprocessed_image = ImgPreprocess.enhance_contrast(preprocessed_image)
+    preprocessed_image = ImgPreprocess.deskew_image(preprocessed_image)
+    preprocessed_image = ImgPreprocess.apply_adaptive_threshold(preprocessed_image)
+    preprocessed_image = ImgPreprocess.localize_text(preprocessed_image)
+    preprocessed_image = ImgPreprocess.resample_image(preprocessed_image, new_width=preprocessed_image_width, new_height=preprocessed_image_height)
+    preprocessed_image = ImgPreprocess.normalize_image(preprocessed_image)
+    preprocessed_image = ImgPreprocess.rotate_image(preprocessed_image)
 
     # Save preprocessed images
     output_path = path.join(Path(output_folder), '.png')
-    save_image(preprocessed_image, output_path)
+    cv2.imwrite(preprocessed_image, output_path)
 
-############# APPLY IMAGE PREPROCESSING FOR THE EXTRACTED IMAGES #############
+############# LOOP THROUGH IMAGE PREPROCESSING STEPS FOR THE EXTRACTED IMAGES #############
 for image in tqdm(listdir(extracted_images_path), desc='Image Preprocessing Progress'):
     # If the output folder doesn't exist, create the folder
-    if not path.exists(extracted_images_path):
-        makedirs(extracted_images_path)
+    if not path.exists(preprocessed_images_path):
+        makedirs(preprocessed_images_path)
 
     preprocess_and_save(image, preprocessed_images_path)
     
