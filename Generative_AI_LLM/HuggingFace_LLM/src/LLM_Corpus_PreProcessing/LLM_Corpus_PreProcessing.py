a5e1e43d3814521eb0d7c957ffd08731fe8ca0c8
Vikram Pande <vikrampande28@gmail.com>
2024-06-21 13:27:59 +1000

    Improved Unicode encode/decode in the PreProcessing module

--- Generative_AI_LLM/HuggingFace_LLM/src/LLM_Corpus_PreProcessing/LLM_Corpus_PreProcessing.py
+++ Generative_AI_LLM/HuggingFace_LLM/src/LLM_Corpus_PreProcessing/LLM_Corpus_PreProcessing.py
@@ -1,16 +1,17 @@
 import json
+import re
+import ftfy
+from ftfy import TextFixerConfig
 from tqdm.auto import tqdm
 from pathlib import Path
 import yaml
 import logging
 from bs4 import BeautifulSoup
 
-clean_list = []
-
 logger = logging.getLogger('Q&A_PreProcessing')
 logger.setLevel(logging.ERROR)
 error_handler = logging.StreamHandler()
-error_handler = logging.FileHandler(Path('C:/Users/Vikram Pande/Side_Projects/Error_Logs/Q&A_PreProcessing_log.log'))
+error_handler = logging.FileHandler(Path('C:/Users/Vikram Pande/Side_Projects_(OUTSIDE_REPO)/Error_Logs/Q&A_PreProcessing_log.log'))
 error_handler.setLevel(logging.ERROR)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 error_handler.setFormatter(formatter)
@@ -18,14 +19,16 @@ logger.addHandler(error_handler)
 
 # Load the file paths and global variables from YAML config file
 try:
-    config_path = Path('C:/Users/Vikram Pande/Side_Projects/Job_Ad_QA_Model')
+    config_path = Path('C:/Users/Vikram Pande/Side_Projects_(OUTSIDE_REPO)/Generative_AI_LLM/HuggingFace_LLM')
 
     with open(config_path / 'config.yml', 'r') as file:
         global_vars = yaml.safe_load(file)
 except:
-    logger.error(f'{config_path} YAML Configuration file path not found. Please check the storage path of the \'config.yml\' file and try again')
+    print(f'{config_path} YAML Configuration file path not found. Please check the storage path of the \'config.yml\' file and try again')
+    # logger.error(f'{config_path} YAML Configuration file path not found. Please check the storage path of the \'config.yml\' file and try again')
 
 # Declare paths for the raw and cleaned JSON files
+clean_list = []
 content_path = global_vars['content_path']
 
 with open(Path(content_path) / 'ads-50k.json', 'r', encoding='utf-8') as f:
@@ -34,16 +37,15 @@ with open(Path(content_path) / 'ads-50k.json', 'r', encoding='utf-8') as f:
 # Declare a function to identify and strip HTML & Unicode tags from the corpus
 def stripTags(input_string):
 
-    string = input_string.encode("ascii", "ignore")
-    string = string.decode()
     string = BeautifulSoup(input_string, 'html.parser')
     string = string.get_text(strip=True)
-    string = (string.replace('\u2022', '').replace('\u00a0', ' ').replace('\u2019', '').replace('\u00b7', '')
-              .replace('\u2013', '').replace('\u2026', '').replace('\u200b', '').replace('\u2018', '')
-              .replace('\u201c', '').replace('\u201d', '').replace('\u00e9', 'e').replace('\uff1a', '')
-              .replace('\u00bd', '').replace('\u0101', '').replace('\u0113', '').replace('\u016b', 'ū')
-              .replace('\u014d', 'ō').replace('\n', ' ').replace('\u00ae', '').replace('\u2122', '')
-              .replace('\u00ad', '').replace('\\', ''))
+    string = string.replace('\n', ' ').replace('\\', ' ')
+    # Heuristic method to fix common issues with unicode
+    string = ftfy.fix_text(string, TextFixerConfig(restore_byte_a0=True, fix_line_breaks=True, decode_inconsistent_utf8=True))
+    string = string.encode('utf-8').decode('unicode_escape')
+    string = string.encode('utf-8').decode('ISO-8859-1')
+    string = string.encode('latin-1').decode('utf-8')
+    string = string.replace('Â', '').replace('Â·', '').replace('â¢', '')
 
     return string
 
